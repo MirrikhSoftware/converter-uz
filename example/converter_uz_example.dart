0f8d6c13fae9bffb288aea1ec627cd4537bbc47d
BahromjonPolat <itjunior.uz@gmail.com>
2023-01-01 11:22:01 +0500

    Licence

--- example/converter_uz_example.dart
+++ example/converter_uz_example.dart
@@ -1,3 +1,8 @@
 import 'package:converter_uz/converter_uz.dart';
 
-void main() {}
+void main() {
+  ConverterUz converter = ConverterUz.instance;
+  print('Assalomu Alaykum!'.convert());
+  converter.setConvertType = ConvertType.ltToCr;
+  print('Assalomu Alaykum!'.convert());
+}
